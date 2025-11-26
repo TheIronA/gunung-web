7954b33cc0adfc7640a4dde206f4349a7caf9c8c
TheIronA <mralwi3@gmail.com>
2025-11-27 02:30:40 +0800

    matching the versions correctly

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 // Don't crash the build if the key is missing, just don't export a valid instance
 export const stripe = process.env.STRIPE_SECRET_KEY
   ? new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: '2024-11-20.acacia', // Use the latest API version or the one you prefer
+      apiVersion: '2025-11-17.clover',
       typescript: true,
     })
   : (null as unknown as Stripe);
